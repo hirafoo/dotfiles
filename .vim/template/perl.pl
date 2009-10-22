d3c58df6baae8bc0d14740bcca56d8020248d24c
hirafoo <hirafoo@cpan.org>
2009-10-22 14:32:50 +0900

    minifix

--- .vim/template/perl.pl
+++ .vim/template/perl.pl
@@ -1,4 +1,4 @@
 use strict;
 use warnings;
-use Data::Dumper;sub p {warn Dumper shift;my @c = caller;print STDERR "  at $c[1]:$c[2]\n\n"}
+use Data::Dumper;sub p {warn Dumper [@_];my @c = caller;print STDERR "  at $c[1]:$c[2]\n\n"}
 
