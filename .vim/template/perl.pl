64863ada30c97c7696bf8183766751b3abc84075
hirafoo <hirafoo@cpan.org>
2011-05-06 14:20:39 +0900

    ref

--- .vim/template/perl.pl
+++ .vim/template/perl.pl
@@ -1,4 +1,4 @@
 use strict;
 use warnings;
 sub say {print @_, "\n"}
-use Data::Dumper;sub p {warn Dumper @_;my @c = caller;print STDERR "  at $c[1]:$c[2]\n\n"}
+use Data::Dumper;sub p {warn Dumper \@_;my @c = caller;print STDERR "  at $c[1]:$c[2]\n\n"}
