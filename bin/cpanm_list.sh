877e07c6fa6550f1c809839f064b06a777aa9cfb
hirafoo <hirafoo@cpan.org>
2011-11-29 15:56:58 +0900

    sort and unique and per_line

--- bin/cpanm_list.sh
+++ bin/cpanm_list.sh
@@ -2,7 +2,67 @@
 
 #curl -L http://cpanmin.us/ | perl - App::cpanminus
 
-for name in Math::BigInt::GMP CGI::Simple::Cookie Data::FormValidator Data::ObjectDriver DBD::mysql DBI Error::Simple FormValidator::Lite Hash::MultiValue HTML::FillInForm HTTP::Status JSON::XS List::Util Net::Server::SS::PreFork Object::Generic::False Plack Plack::Handler::Starman Plack::Middleware::ReverseProxy Plack::Runner Router::Simple Router::Simple::Declare Server::Starter TheSchwartz TheSchwartz::Job TheSchwartz::Simple TheSchwartz::Simple::Job TheSchwartz::Worker Time::HiRes XML::OPML XML::Parser XML::Parser::Expat YAML::XS Encode::JavaScript::UCS Crypt::DH Net::CIDR::Lite Server::Starter Plack::Middleware::ReverseProxy Net::Server::SS::PreFork Linux::Inotify2 XML::Feed HTML::FillInForm::Lite Text::Xslate Cache::Memcached::Fast Clone Config::Pit Crypt::CBC Crypt::DH Data::Page::Navigation Email::Valid::Loose File::Slurp HTML::Template Math::BigInt::GMP Module::Pluggable::Fast Parallel::Scoreboard String::CamelCase Time::Out Time::Piece XMLRPC::Lite Plack::Middleware::Session Digest::SHA1 Digest::SHA App::Ack Text::Xslate::Bridge::TT2Like Net::SSLeay Crypt::SSLeay
+for name in
+    App::Ack
+    CGI::Simple::Cookie
+    Cache::Memcached::Fast
+    Clone
+    Config::Pit
+    Crypt::CBC
+    Crypt::DH
+    Crypt::SSLeay
+    DBD::mysql
+    DBI
+    Data::FormValidator
+    Data::ObjectDriver
+    Data::Page::Navigation
+    Digest::SHA
+    Digest::SHA1
+    Email::Valid::Loose
+    Encode::JavaScript::UCS
+    Error::Simple
+    File::Slurp
+    FormValidator::Lite
+    HTML::FillInForm
+    HTML::FillInForm::Lite
+    HTML::Template
+    HTTP::Status
+    Hash::MultiValue
+    JSON::XS
+    Linux::Inotify2
+    List::Util
+    Math::BigInt::GMP
+    Module::Pluggable::Fast
+    Net::CIDR::Lite
+    Net::SSLeay
+    Net::Server::SS::PreFork
+    Object::Generic::False
+    Parallel::Scoreboard
+    Plack
+    Plack::Handler::Starman
+    Plack::Middleware::ReverseProxy
+    Plack::Middleware::Session
+    Plack::Runner
+    Router::Simple
+    Router::Simple::Declare
+    Server::Starter
+    String::CamelCase
+    Text::Xslate
+    Text::Xslate::Bridge::TT2Like
+    TheSchwartz
+    TheSchwartz::Job
+    TheSchwartz::Simple
+    TheSchwartz::Simple::Job
+    TheSchwartz::Worker
+    Time::HiRes
+    Time::Out
+    Time::Piece
+    XML::Feed
+    XML::OPML
+    XML::Parser
+    XML::Parser::Expat
+    XMLRPC::Lite
+    YAML::XS
 do
     cpanm --skip-installed $name
 done
