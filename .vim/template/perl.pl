1107388a515820ed52434188f937c7578cf7a71d
hirafoo <hirafoo@cpan.org>
2020-12-10 21:33:49 +0900

    perl tmpl

--- .vim/template/perl.pl
+++ .vim/template/perl.pl
@@ -1,4 +1,4 @@
 use strict;
 use warnings;
-sub say {print @_, "\n"}
+use v5.10;
 use Data::Dumper;sub p {local $Data::Dumper::Sortkeys=1; warn Dumper \@_;my @c = caller;warn "  at $c[1]:$c[2]\n\n"}
